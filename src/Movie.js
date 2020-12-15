b3584ed0fe996fec425df623ad74b1f49e9e953f
unknown <jjw1578@gmail.com>
2020-12-15 16:53:59 +0900

    updtae

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({id,year,title,summary,poster}){
-    return<h1>{title}</h1>
+function Movie({id,year,title,summary,poster,rating}){
+    return(
+        <div>
+            <h4>{title} {rating}/10.0</h4>
+            <img src={poster} alt={title}/>
+        </div>
+    );
 } 
 
 Movie.propTypes = {
     id:PropTypes.number.isRequired,
     year:PropTypes.number.isRequired,
+    rating:PropTypes.number.isRequired,
     title:PropTypes.string.isRequired,
     summary:PropTypes.string.isRequired,
     poster:PropTypes.string.isRequired
