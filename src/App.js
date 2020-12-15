5015b035fec1c6f63b20ed77eb5bad50d7d3d0cf
unknown <jjw1578@gmail.com>
2020-12-15 15:42:41 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,40 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie.js";
+//import PropTypes from "prop-types";
 import "./style.css";
 
-
-const FoodILike = [
-  {
-    id:1,
-    name : "Kamjatang",
-    image:
-    "https://homecuisine.co.kr/files/attach/images/140/990/086/3555cb39cb6066ed9c092d1b77975719.JPG",
-    rating:5.0
-  },
-  {
-    id:2,
-    name : "Galbi",
-    image:
-    "http://mlb26.cafe24.com/img/galbi/la_galbi_gui.png",
-    rating:5.0
-  },
-  {
-    id:3,
-    name:"KimchiJJim",
-    image:
-    "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/21/8cb4b9f30e3570f4ff93dd3303eeff7f1.jpg",
-    rating:5.0
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies:[]
+  };
+  getMovies=async()=>{
+    const {data :{data:{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies,isLoading:false});
+  };
+  componentDidMount(){
+    this.getMovies();
   }
-];
-
-function Food({name,picture,rating}){
-  return(
-    <div>
-      <h2>I like {name}!</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
+  render(){
+    const { isLoading,movies} = this.state;
+    return (
+      <div>
+        {isLoading 
+          ? "Loading ..."
+          :movies.map(movie =>(
+                <Movie 
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
     </div>
-  );
+    );
+  }
 }
 
-Food.propTypes = {
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-};
-
-
-function App() {
-  return(
-    <div>
-      {FoodILike.map(dish => (
-        <Food 
-        key={dish.id}
-        name={dish.name}
-        picture={dish.image}
-        rating ={dish.rating}
-        className="photo"/>
-        ))}
-    </div>
-  );
-}
 
 export default App;
